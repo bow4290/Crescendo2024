7f62152960a271f01dafcd97b2fa84f166c08c52
Lexi <76599467+JumpingPistachio@users.noreply.github.com>
2024-02-12 20:50:29 -0500

    Add SmartDashboard values to robot (so far)

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -34,6 +35,13 @@ public class Intake extends SubsystemBase {
       () -> {
         motorIntake.stopMotor();
       });
+
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Current Intake Output", motorIntake.get());
+    SmartDashboard.putNumber("Current Intake Velocity", motorIntake.getVelocity().getValueAsDouble());
   }
     
 }
