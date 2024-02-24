0a42492114ea6d0c2e88db4ffd48d6f01e67076d
Lexi <76599467+JumpingPistachio@users.noreply.github.com>
2024-02-24 10:59:08 -0500

    add command for going to state in WrivotStates

--- src/main/java/frc/robot/subsystems/WrivotStates.java
+++ src/main/java/frc/robot/subsystems/WrivotStates.java
@@ -4,11 +4,11 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.lib.math.Conversions;
@@ -131,6 +131,15 @@ public class WrivotStates extends SubsystemBase {
             return wristDegrees;
         }
 
+    }
+    // start of non-enum code (checkpoint)
+
+    public Command goToStateCommand(BotAngleState setState){
+      return this.startEnd(() -> {
+        if (setState != BotAngleState.AIMING && setState != BotAngleState.INTERMEDIATE){
+          wrivotSequencer(setState.getPivotDegrees(), setState.getWristDegrees());
+        }
+      }, () -> {});
     }
 
     public BotAngleState getCurrentState(){
