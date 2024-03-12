3249b35d6b6dd7b345a52d4ea70b1ac0bf701352
Lexi <76599467+JumpingPistachio@users.noreply.github.com>
2024-03-11 20:00:23 -0400

    Climber no more PID cause why

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -19,13 +20,13 @@ public class Climber extends SubsystemBase {
   
   public static final double GEAR_RATIO_CLIMBER = 32/1;
 
-  public static final double CLIMBER_UP_SPEED_RPM = 90;
-  public static final double CLIMBER_DOWN_SPEED_RPM = 70;
+  public static final double CLIMBER_UP_SPEED = 0.4;
+  public static final double CLIMBER_DOWN_SPEED = 0.6;
 
   final TalonFX motorClimber1 = new TalonFX(MOTOR_ID_CLIMBER_1);
   final TalonFX motorClimber2 = new TalonFX(MOTOR_ID_CLIMBER_2);
 
-  final VelocityVoltage velocityRequest = new VelocityVoltage(0).withSlot(0);
+  final DutyCycleOut dutyCycleOut = new DutyCycleOut(0);
 
   public Climber(){
     TalonFXConfiguration climberConfiguration = new TalonFXConfiguration();
@@ -34,23 +35,16 @@ public class Climber extends SubsystemBase {
     
     climberConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
     
-    climberConfiguration.Slot0.kS = 0.05;
-    climberConfiguration.Slot0.kV = 0.12;
-    climberConfiguration.Slot0.kP = 0.11;
-    climberConfiguration.Slot0.kI = 0.0;
-    climberConfiguration.Slot0.kD = 0.0;
-    
     motorClimber1.getConfigurator().apply(climberConfiguration);
     motorClimber2.getConfigurator().apply(climberConfiguration);
     
     motorClimber1.setInverted(true);
   }
 
-  public Command cmdClimbSide(double velocityRPM, TalonFX targetMotor, WrivotStates reqWrivotStates){
-    double velocityRPS = (velocityRPM * 60) * GEAR_RATIO_CLIMBER;
+  public Command cmdClimbSide(double targetOut, TalonFX targetMotor, WrivotStates reqWrivotStates){
 
     StartEndCommand cmd = new StartEndCommand(() -> {
-      targetMotor.setControl(velocityRequest.withVelocity(velocityRPS));
+      targetMotor.setControl(dutyCycleOut.withOutput(targetOut));
     }, 
     () -> {
       targetMotor.stopMotor();
@@ -59,12 +53,11 @@ public class Climber extends SubsystemBase {
     return cmd;
   }
 
-  public Command cmdClimbTogether(double velocityRPM, WrivotStates reqWrivotStates){
-    double velocityRPS = (velocityRPM * 60) * GEAR_RATIO_CLIMBER;
+  public Command cmdClimbTogether(double targetOut, WrivotStates reqWrivotStates){
 
     StartEndCommand cmd = new StartEndCommand(() -> {
-      motorClimber1.setControl(velocityRequest.withVelocity(velocityRPS));
-      motorClimber2.setControl(velocityRequest.withVelocity(velocityRPS));
+      motorClimber1.setControl(dutyCycleOut.withOutput(targetOut));
+      motorClimber2.setControl(dutyCycleOut.withOutput(targetOut));
     }, 
     () -> {
       motorClimber1.stopMotor();
