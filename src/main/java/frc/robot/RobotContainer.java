ec4be9d41d245fa0be926aa8aa2d4898c0e58e57
IcyMagicCrystal <IcyMagicCrystal@Gmail.com>
2024-03-20 11:26:12 -0400

    IndexerButtonWorks?
    Added Indexer Button as a trigger which would trigger the LEDs to flash blue when the button is pushed. Set buttons DIO port to 0.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -50,6 +51,9 @@ public class RobotContainer {
         // Configure the button bindings
         Controls.configureDriver(this);
         Controls.configureOperator(this);
+
+        //Instantiates indexer button,  I think
+        Shooter.indexerTrigger.whileTrue(led.setLedsToFlashColor(Color.kAliceBlue));
 
         // Register Name Commands
         NamedCommands.registerCommand("Intake Full", Commands.parallel(
