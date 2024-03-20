a0a1b8c0c55dbc36b63a4a928623bb6cc4c250b6
IcyMagicCrystal <IcyMagicCrystal@Gmail.com>
2024-03-20 10:35:38 -0400

    Add LED Flashing
    Add a way to alternate flashing the LEDs to a set color.

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class LED extends SubsystemBase{
@@ -12,13 +13,15 @@ public class LED extends SubsystemBase{
   private static final int ledLength = 50;
   private final AddressableLED led = new AddressableLED(pwmPort);
   private AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(ledLength);
-  private double totalBrightness;
-  private final double maxTotalBrightness = 1550;
-  private double brightnessDivider;
+  private final double maxTotalBrightness = 1500;
 
   public LED() {
     led.setLength(ledLength);
     led.start();
+  }
+
+  public Command setLedsToSolidColor(Color color){
+    return runLeds(() -> setLedBufferToSolidColor(color));
   }
 
   public void setLedBufferToSolidColor(Color color){
@@ -27,8 +30,17 @@ public class LED extends SubsystemBase{
     }
   }
 
-  public Command setLedsToSolidColor(Color color){
-    return runLeds(() -> setLedBufferToSolidColor(color));
+  public Command setLedsToFlashColor(Color color){
+    return runLeds(() -> setLedBufferToFlashColor(color));
+  }
+
+  public void setLedBufferToFlashColor(Color color){
+    int seconds = (int)Timer.getFPGATimestamp();
+    for (int ledIndex = 0; ledIndex < ledLength; ledIndex++) {
+      if (ledIndex % 2 == seconds % 2) {
+        ledBuffer.setLED(ledIndex, color);
+      }
+    }
   }
 
   public Command runLeds(Runnable action) {
@@ -46,14 +58,20 @@ public class LED extends SubsystemBase{
   }
 
   public void powerLimitLeds() {
-    totalBrightness = 0;
+    double totalBrightness = 0;
     for (int ledIndex = 0; ledIndex < ledLength; ledIndex++) {
-      totalBrightness += ledBuffer.getLED(ledIndex).red + ledBuffer.getLED(ledIndex).green + ledBuffer.getLED(ledIndex).blue;
+      totalBrightness += 
+      ledBuffer.getLED(ledIndex).red + 
+      ledBuffer.getLED(ledIndex).green + 
+      ledBuffer.getLED(ledIndex).blue;
     }
-    brightnessDivider = totalBrightness/maxTotalBrightness;
+    double brightnessDivider = totalBrightness/maxTotalBrightness;
     if(maxTotalBrightness>1) {
       for (int ledIndex = 0; ledIndex < ledLength; ledIndex++) {
-        ledBuffer.setLED(ledIndex, new Color(ledBuffer.getLED(ledIndex).red/brightnessDivider, ledBuffer.getLED(ledIndex).green/brightnessDivider, ledBuffer.getLED(ledIndex).blue/brightnessDivider));
+        ledBuffer.setLED(ledIndex, new Color(
+          ledBuffer.getLED(ledIndex).red/brightnessDivider, 
+          ledBuffer.getLED(ledIndex).green/brightnessDivider, 
+          ledBuffer.getLED(ledIndex).blue/brightnessDivider));
       }
     }
 
